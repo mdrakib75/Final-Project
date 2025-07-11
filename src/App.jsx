769a516269c0902697df290258f1ca8694c2390b
mdrakib75 <mdrakib2780180@gmail.com>
2025-07-11 21:23:55 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1 className='text-green-500 font-bold text-8xl font-dms text-center'>Rakib</h1>
+    </>
+  )
+}
+
+export default App
