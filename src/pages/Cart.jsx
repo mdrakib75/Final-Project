b03ff5500b7f99a052cfe5dbf88e267ee55b46dc
mdrakib75 <mdrakib2780180@gmail.com>
2025-09-24 22:59:11 +0600

    Redux

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,11 +3,18 @@ import Container from "../Components/Container";
 import { Link } from "react-router-dom";
 import { FaChevronRight } from "react-icons/fa";
 import productImg from "../assets/productImg.png";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { cartQuantity } from "../slice/cartSlice";
 
 const Cart = () => {
+  const dispatch = useDispatch()
   const data = useSelector((state) => state.cartDetails.cartItems);
   console.log(data ,"Data");
+
+  const handleIncrement = (item) =>{
+    console.log("Ok",item);
+    dispatch(cartQuantity(item));
+  }
   return (
     <div>
       <Container>
@@ -49,28 +56,34 @@ const Cart = () => {
             <div className="w-[25%] font-bold font-dms text-primary">Total</div>
           </div>
 
-  <div className=" flex justify-between items-center py-[34px] px-[20px]">
+          {
+            data.length > 0 ?
+             data?.map((item) => (
+           <div  className=" flex justify-between items-center py-[34px] px-[20px]">
               <div className="flex items-center w-[25%] font-bold font-dm">
-                <img className="w-[70px]" src={productImg} alt="" />
-                <p>Product name</p>
+                <img className="w-[70px] bg-[#D8D8D8]" src={item.thumbnail} alt="" />
+                <p className="pl-3">{item.title}</p>
               </div>
               <div className="w-[25%] font-bold font-dms text-[20px] text-primary">
-                $44.00
+                ${item.price}
               </div>
               <div className="w-[25%] font-bold font-dms text-primary">
                 <div className="flex items-center border-2 border-[#F0F0F0] w-fit px-[21px] space-x-[35px]">
-                  <p>-</p>
-                  <p>1</p>
-                  <p>+</p>
+                  <p className="cursor-pointer">-</p>
+                  <p>{item.cartQun}</p>
+                  <p className="cursor-pointer" onClick={()=>handleIncrement(item)}>+</p>
                 </div>
               </div>
               <div className="w-[25%] font-bold font-dms text-[20px] text-primary">
-                $44.00
+                ${item.price * item.cartQun}
               </div>
             </div>
-          {/* {data.map((item) => (
-          
-          ))} */}
+          ))
+          : "no items"
+          }
+
+         
+
         </div>
       </Container>
     </div>
--- src/slice/cartSlice.js
+++ src/slice/cartSlice.js
@@ -10,19 +10,23 @@ export const cartSlice = createSlice({
         // console.log(state.cartItems);
         // console.log(action.payload, "action");
         // state.cartItems.push(action.payload)
-        const findIndex = state.cartItems.findIndex((item) => item.id == action.payload.id)
-        if(findIndex >= 0){
-          state.cartItems[findIndex].cartQun += 1
+        const checkIndex = state.cartItems.findIndex((item) => item.id == action.payload.id)
+        if(checkIndex >= 0){
+          state.cartItems[checkIndex].cartQun += 1
         }
         else{
           state.cartItems.push({...action.payload, cartQun: 1})
         } 
-        localStorage.setItem("cartDetails", JSON.stringify(action.payload))
+        localStorage.setItem("cartDetails", JSON.stringify({...action.payload}))
     //   state.value += 1
+    },
+    cartQuantity:(state,action)=>{
+      console.log(state);
+      console.log(action.payload);
     }
   },
 })
 
-export const { cartTotal } = cartSlice.actions
+export const { cartTotal , cartQuantity} = cartSlice.actions
 
 export default cartSlice.reducer
